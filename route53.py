87b8be9179b2c9c1b157d2d7760f578ab823c6bd
Kazuya Takei <attakei@gmail.com>
2017-07-25 11:38:43 +0900

    Require IAM user key pair command

--- route53.py
+++ route53.py
@@ -1,7 +1,35 @@
-from errbot import BotPlugin
+# -*- coding:utf8 -*-
+import functools
+import textwrap
+from errbot import BotPlugin, botcmd
+
+
+def require_iam(f):
+    """Deforator for bot command required IAM user key pair
+    """
+    @functools.wraps(f)
+    def _require_iam(bot, msg, args):
+        if not bot.config \
+                or not bot.config.get('access_id', None) \
+                or not bot.config.get('secret_key', None):
+            return bot.not_configured()
+    return _require_iam
 
 
 class Route53(BotPlugin):
     """
     Control Route 53
     """
+
+    def not_configured(self):
+        message = """
+            This plugin is until not configured.
+            Please call `{}plugin config route53` to read format,
+            And set your configurations.
+            """
+        return textwrap.dedent(message).format(self.bot_config.BOT_PREFIX)
+
+    @botcmd
+    @require_iam
+    def route53_list(self, msg, args):
+        pass
--- test_plugin.py
+++ test_plugin.py
@@ -10,3 +10,9 @@ extra_plugin_dir = '.'
 def test_installed_plugin(testbot):
     testbot.push_message('!status plugins')
     assert 'route53' in testbot.pop_message()
+
+
+def test_not_configured(testbot):
+    testbot.push_message('!route53 list')
+    assert 'This plugin is until not configured' \
+        in testbot.pop_message()
