964525570deb2d99c2142b86baa1270b5d2f9aa0
Kazuya Takei <attakei@gmail.com>
2017-09-12 15:56:54 +0900

    Implement proc

--- route53.py
+++ route53.py
@@ -93,4 +93,27 @@ class Route53(BotPlugin):
     @arg_botcmd('name')
     @arg_botcmd('zone_id')
     def route53_add_record(self, msg, zone_id, name, record_type, record_value):
+        if not self.has_iam():
+            return self.not_configured()
+        zone_id = zone_id[12:]
+        client = self.get_client()
+        result = client.change_resource_record_sets(
+            HostedZoneId=zone_id,
+            ChangeBatch={
+                'Comment': 'Add new record',
+                'Changes': [
+                    {
+                        'Action': 'CREATE',
+                        'ResourceRecordSet': {
+                            'Name': name,
+                            'Type': record_type,
+                            'TTL': 300,
+                            'ResourceRecords': [
+                                {'Value': record_value}
+                            ],
+                        }
+                    }
+                ]
+            }
+        )
         return 'Now creating'
